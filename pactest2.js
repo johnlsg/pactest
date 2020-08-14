3c1c7d45193677443ecb20d4f1c2c78e95f0cf06
John Law <johnlsg9909@gmail.com>
2020-08-14 13:28:25 +0800

    update

---
+++ pactest2.js
@@ -0,0 +1,17 @@
+function FindProxyForURL(url, host) {
+    if (dnsDomainIs(host, ".heavenlyfood.cn") || shExpMatch(host, "(*heavenlyfood.cn)")){
+        return "PROXY 223.82.106.253:3128";
+
+    }
+
+
+
+    if (url.match(".*heavenlyfood\\.cn")) {
+
+
+        return "PROXY 223.82.106.253:3128; PROXY 114.99.8.248:4216; PROXY 60.169.201.13:53315; PROXY 113.100.209.140:3128; PROXY 59.36.10.79:3128; PROXY 175.6.66.48:3128";
+
+    } else {
+        return "DIRECT";
+    }
+}
